9a1de2bdff9076e42ac4d003329a7ca55ef8e6e1
BrookeLeeK <kalmanbrooke@yahoo.com>
2022-06-04 22:04:27 -0500

    changes

--- server.js
+++ server.js
@@ -7,22 +7,22 @@ app.use(cors())
 
 const plants = {
     'succulent': {
-        'name': 'succulent',
+        'name': 'Succulent',
         'light': '6 hours of sun per day depending on the type.',
-        'rotate': 'rotate them often',
-        'water': 'test the soil with a finger, when the top 1.25 inches are dry grab your watering can and water them',
+        'rotate': 'rotate them often.',
+        'water': 'Test the soil with a finger, when the top 1.25 inches are dry grab your watering can and water them.',
     },
     'cactus': {
-        'name': 'cactus',
-        'light': 'direct light all day, they like temps above 50 degrees F',
-        'rotate': 'rotate every now and then',
-        'water': 'check soil every few weeks, if the first 2-3 inches of soil are dry its time to water'
+        'name': 'Cactus',
+        'light': 'Direct light all day, they like temps above 50 degrees F',
+        'rotate': 'Rotate every now and then',
+        'water': 'Check soil every few weeks, if the first 2-3 inches of soil are dry its time to water'
     },
     'monstera': {
-        'name' : 'monstera',
-        'light' : 'bright to medium indirect light',
-        'rotate' : 'a quarter turn everywhere from every 3 days to every 2 weeks depending on how much the plant is leaning',
-        'water' : 'water every 1-2 weeks, allow soil to dry out between waterings'
+        'name' : 'Monstera',
+        'light' : 'Bright to medium indirect light',
+        'rotate' : 'A quarter turn everywhere from every 3 days to every 2 weeks depending on how much the plant is leaning',
+        'water' : 'Water every 1-2 weeks, allow soil to dry out between waterings'
     }
 }
 
